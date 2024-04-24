28cfaa9d79d11bd4eece8f72dd57432220b0655a
Abdurrahman Abdulaziz <abdelrahman_omar_1515@yahoo.com>
2024-04-25 00:24:38 +0200

    scan identifiers and numbers

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Clone)]
 enum TokenType {
     // Single-character tokens.
@@ -76,12 +78,34 @@ pub struct Scanner {
     start: usize,
     current: usize,
     line: usize,
+
+    keywords: HashMap<String, TokenType>,
 }
 
 impl Scanner {
     pub fn new(source: String) -> Self {
+        let keywords = HashMap::from([
+            ("and".to_string(), TokenType::And),
+            ("class".to_string(), TokenType::Class),
+            ("else".to_string(), TokenType::Else),
+            ("false".to_string(), TokenType::False),
+            ("for".to_string(), TokenType::For),
+            ("fun".to_string(), TokenType::Fun),
+            ("if".to_string(), TokenType::If),
+            ("nil".to_string(), TokenType::Nil),
+            ("or".to_string(), TokenType::Or),
+            ("print".to_string(), TokenType::Print),
+            ("return".to_string(), TokenType::Return),
+            ("super".to_string(), TokenType::Super),
+            ("this".to_string(), TokenType::This),
+            ("true".to_string(), TokenType::True),
+            ("var".to_string(), TokenType::Var),
+            ("while".to_string(), TokenType::While),
+        ]);
+
         Self {
             source: source.chars().collect(),
+            keywords,
             ..Default::default()
         }
     }
@@ -154,6 +178,9 @@ impl Scanner {
                     self.add_token(TokenType::Slash, None);
                 }
             }
+            '"' => self.string(),
+            c if c.is_ascii_digit() => self.number(),
+            c if c.is_alphabetic() => self.identifier(),
             ' ' | '\r' | '\t' => (),
             '\n' => self.line += 1,
             _ => {
@@ -185,6 +212,62 @@ impl Scanner {
             return '\0';
         }
         self.source[self.current]
+    }
+
+    fn peek_next(&self) -> char {
+        if self.current + 1 >= self.source.len() {
+            return '\0';
+        };
+        self.source[self.current + 1]
+    }
+
+    fn string(&mut self) {
+        while self.peek() != '"' && !self.at_end() {
+            if self.peek() == '\n' {
+                self.line += 1
+            }
+            self.advance();
+        }
+        if self.at_end() {
+            // TODO: Better error handling
+            eprintln!("Unterminated string");
+            return;
+        }
+
+        // The closing "
+        self.advance();
+
+        let literal = &self.source[self.start + 1..self.current - 1];
+        self.add_token(TokenType::String, Some(literal.iter().collect()));
+    }
+
+    fn number(&mut self) {
+        while self.peek().is_ascii_digit() {
+            self.advance();
+        }
+        if self.peek() == '.' && self.peek_next().is_ascii_digit() {
+            self.advance();
+            while self.peek().is_ascii_digit() {
+                self.advance();
+            }
+        }
+
+        let num = &self.source[self.start..self.current];
+        // NOTE: Not parsed into a number
+        self.add_token(TokenType::Number, Some(num.iter().collect()));
+    }
+
+    fn identifier(&mut self) {
+        while self.peek().is_alphanumeric() {
+            self.advance();
+        }
+        let text = &self.source[self.start..self.current];
+        let token_type = self
+            .keywords
+            .get(&text.iter().collect::<String>())
+            .unwrap_or(&TokenType::Identifier)
+            .to_owned();
+        self.add_token(token_type, None);
     }
 
     fn add_token(&mut self, token_type: TokenType, literal: Option<String>) {
